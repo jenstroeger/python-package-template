079b801c14d6fc6238c6dc5f1df4aa70870e409d
Jens Troeger <jens.troeger@light-speed.de>
2022-05-26 13:22:25 +1000

    fix: remove exit code because they’re Unix only

--- src/package/__main__.py
+++ src/package/__main__.py
@@ -4,7 +4,6 @@
 # https://docs.python.org/3/library/runpy.html
 # https://docs.python.org/3/reference/import.html#special-considerations-for-main
 
-import os
 import sys
 
 from .something import Something
@@ -17,4 +16,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-    sys.exit(os.EX_OK)
+    sys.exit()  # No exit() argument means successful termination.
