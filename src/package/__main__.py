be7ba064192624558fbf97c2dea37a7189ccb3ad
Jens Tröger <jenstroeger@users.noreply.github.com>
2022-05-29 02:56:30 +1000

    feat: move package specification, tool configs to pyproject.toml (#208)

--- src/package/__main__.py
+++ src/package/__main__.py
@@ -4,17 +4,16 @@
 # https://docs.python.org/3/library/runpy.html
 # https://docs.python.org/3/reference/import.html#special-considerations-for-main
 
-import os
 import sys
 
 from .something import Something
 
 
-def main():
+def main() -> None:
     """Execute the Something standalone command-line tool."""
     _ = Something.do_something()
 
 
 if __name__ == "__main__":
     main()
-    sys.exit(os.EX_OK)
+    sys.exit()  # No exit() argument means successful termination.
