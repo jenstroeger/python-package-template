ce96c4518862a5043b95cfe9b6f315803785828f
Behnaz Hassanshahi <b.hassanshahi@gmail.com>
2022-06-22 09:23:48 +1000

    refactor(ci): refactor and address security issues in workflows (#211)

--- commitlint.rules.js
+++
@@ -1,7 +0,0 @@
-// See also .github/workflows/pull-request.yaml
-module.exports = {
-  rules: {
-    "subject-max-length": [2, "always", 100],
-    "body-max-line-length": [2, "always", 200],
-  },
-};
