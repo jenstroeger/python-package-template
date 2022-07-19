92a64ff294db8f8bd26f1cf2bdf69d4dd920e9c1
Jens Troeger <jens.troeger@light-speed.de>
2022-07-19 21:08:01 +1000

    chore: update copyright years

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath("../../src"))
 # -- Project information -----------------------------------------------------
 
 project = "Package"
-copyright = "2021, Package"
+copyright = "2022, Package"
 author = ""
 
 
