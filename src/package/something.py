a3af1c30a6869432de35d52021a07994f9e6c7d2
Jens Troeger <jens.troeger@light-speed.de>
2022-10-10 08:33:17 +1000

    fix: unbreak the code 😇

--- src/package/something.py
+++ src/package/something.py
@@ -5,6 +5,6 @@ class Something:
     """The Something class provides some things."""
 
     @staticmethod
-    def do_something(value: bool = False) -> str:
+    def do_something(value: bool = False) -> bool:
         """Return true, always."""
         return value or True
