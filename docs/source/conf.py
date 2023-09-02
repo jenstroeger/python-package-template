e2d3fd2f4ac8e28ad5e2788be56ef3da176b7538
Jens Tröger <jenstroeger@users.noreply.github.com>
2023-09-02 17:49:57 +1000

    refactor(docs): switch to sphinx-markdown-builder package (#585)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
+    "sphinx_markdown_builder",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
