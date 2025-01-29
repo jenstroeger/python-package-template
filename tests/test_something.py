310906798dd408fc8b0d002eb6e42c197e85cb27
Jens Troeger <jens.troeger@light-speed.de>
2025-01-29 17:52:58 +1000

    chore: use Faker to produce test case data in the case function

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,7 +1,8 @@
 """Test the Something module. Add more tests here, as needed."""
 
+import faker
 from hypothesis import given, strategies
-from pytest_cases import parametrize
+from pytest_cases import parametrize_with_cases
 
 from package.something import Something
 
@@ -12,7 +13,12 @@ def test_something_hypothesis(boolean: bool) -> None:
     assert Something.do_something(boolean) is True
 
 
-@parametrize("boolean", [True, False])
+def _case_boolean() -> bool:
+    fake = faker.Faker()
+    return fake.pybool()
+
+
+@parametrize_with_cases("boolean", cases=_case_boolean)
 def test_something_cases(boolean: bool) -> None:
     """Test something here using Cases."""
     assert Something.do_something(boolean) is True
