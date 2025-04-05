b868bba3a7ce03719b50e0f40c9d5558ef7a2c99
Jens Tröger <jenstroeger@users.noreply.github.com>
2025-04-05 18:21:12 +1000

    feat(test): add the pytest-cases package and a simple example of how to use it (#873)

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,11 +1,24 @@
 """Test the Something module. Add more tests here, as needed."""
 
+import faker
 from hypothesis import given, strategies
+from pytest_cases import parametrize_with_cases
 
 from package.something import Something
 
 
 @given(strategies.booleans())
-def test_something(boolean: bool) -> None:
-    """Test something here."""
+def test_something_hypothesis(boolean: bool) -> None:
+    """Test something here using Hypothesis."""
+    assert Something.do_something(boolean) is True
+
+
+def _case_boolean() -> bool:
+    fake = faker.Faker()
+    return fake.pybool()
+
+
+@parametrize_with_cases("boolean", cases=_case_boolean)
+def test_something_cases(boolean: bool) -> None:
+    """Test something here using Cases and Faker."""
     assert Something.do_something(boolean) is True
