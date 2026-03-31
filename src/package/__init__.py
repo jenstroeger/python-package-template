311cc46676c189045754d3c476bdff466d4b5b6d
Jens Troeger <jens.troeger@light-speed.de>
2026-03-31 19:29:26 +1000

    fix: use proper SPDX license identifier in `pyproject.toml` and add correct copyright comments to package source and tests

--- src/package/__init__.py
+++ src/package/__init__.py
@@ -3,6 +3,9 @@
 This package is an empty cookiecutter package example. Enjoy!
 """
 
+# Copyright (c) 2021-2026 CODEOWNERS
+# This code is licensed under MIT license, see LICENSE.md for details.
+
 # The version of this package. There's no comprehensive, official list of other
 # magic constants, so we stick with this one only for now. See also this conversation:
 # https://stackoverflow.com/questions/38344848/is-there-a-comprehensive-table-of-pythons-magic-constants
